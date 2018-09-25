0ad82391645e7faa98770925384c3d556c0c390c
Victor Grigoriu <vgrigoriu@gmail.com>
2018-09-25 22:40:03 +0300

    can scan left parens

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace lox.cs
+{
+    class Lox
+    {
+        private static bool hadError;
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: lox.cs [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string path)
+        {
+            var source = File.ReadAllText(path, Encoding.UTF8);
+            Run(source);
+            if (hadError)
+            {
+                Environment.Exit(65);
+            }
+        }
+
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                Run(Console.ReadLine());
+                hadError = false;
+            }
+        }
+
+        private static void Run(string source)
+        {
+            var scanner = new Scanner(source);
+            var tokens = scanner.ScanTokens();
+
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        static internal void Error(int line, string message)
+        {
+            Report(line, string.Empty, message);
+        }
+
+        private static void Report(int line, string where, string message)
+        {
+            Console.Error.WriteLine($"[line {line}] Error{where}: {message}");
+            hadError = true;
+        }
+    }
+}
---
+++ Scanner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace lox.cs
+{
+    internal class Scanner
+    {
+        private readonly string source;
+        private readonly List<Token> tokens = new List<Token>();
+        private int start = 0;
+        private int current = 0;
+        private int line = 1;
+
+        public Scanner(string source)
+        {
+            this.source = source;
+        }
+
+        internal IEnumerable<Token> ScanTokens()
+        {
+            while (!IsAtEnd())
+            {
+                // We are at the beginning of the next lexeme
+                start = current;
+                ScanToken();
+            }
+
+            tokens.Add(new Token(TokenType.EOF, string.Empty, null, line));
+            return tokens;
+        }
+
+        private void ScanToken()
+        {
+            var c = Advance();
+            switch (c)
+            {
+                case '(':
+                    AddToken(TokenType.LEFT_PAREN);
+                    break;
+            }
+        }
+
+        private char Advance()
+        {
+            current++;
+            return source[current - 1];
+        }
+
+        private void AddToken(TokenType type)
+        {
+            AddToken(type, null);
+        }
+
+        private void AddToken(TokenType type, object literal)
+        {
+            var text = source.Substring(start, current - start);
+            tokens.Add(new Token(type, text, literal, line));
+        }
+
+        private bool IsAtEnd()
+        {
+            return current >= source.Length;
+        }
+    }
+}
